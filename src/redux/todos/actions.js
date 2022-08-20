21c72810908ce5cf1a02b32c41f446c7f66fe1fc
Rasujon3 <rasujon3@gmail.com>
2022-08-21 00:31:59 +0600

    Defining Actions done

---
+++ src/redux/todos/actions.js
@@ -0,0 +1,51 @@
+import {
+  ADDED,
+  ALLCOMPLETED,
+  CLEARCOMPLETED,
+  COLORSELECTED,
+  DELETEDED,
+  TOGGLED,
+} from "./actionTypes";
+
+export const added = (todoText) => {
+  return {
+    type: ADDED,
+    payload: todoText,
+  };
+};
+
+export const toggled = (todoId) => {
+  return {
+    type: TOGGLED,
+    payload: todoId,
+  };
+};
+
+export const colorSelected = (todoId, color) => {
+  return {
+    type: COLORSELECTED,
+    payload: {
+      todoId,
+      color,
+    },
+  };
+};
+
+export const deleted = (todoId) => {
+  return {
+    type: DELETEDED,
+    payload: todoId,
+  };
+};
+
+export const allCompleted = () => {
+  return {
+    type: ALLCOMPLETED,
+  };
+};
+
+export const clearCompleted = () => {
+  return {
+    type: CLEARCOMPLETED,
+  };
+};
